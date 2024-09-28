575bbfe6caaec7f49d5f96babe85c3342ed2d78e
Ashutosh Pandey <ashutosh3276s16@gmail.com>
2024-09-28 18:08:52 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_ai/screens/student_portal_screen_large.dart';
 import 'package:quiz_ai/screens/student_portal_screen_medium.dart';
@@ -9,7 +10,9 @@ import 'screens/Create_Quiz.dart';
 import 'screens/HomePage.dart';  // Import HomePage for Bottom Navigation
 import 'screens/my_quizzes_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -15,6 +16,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   // Google sign-in instance
   final GoogleSignIn _googleSignIn = GoogleSignIn();
@@ -28,6 +30,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   void dispose() {
     _passwordController.dispose();
     _confirmPasswordController.dispose();
+    _emailController.dispose();
     super.dispose();
   }
 
@@ -65,6 +68,26 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
   }
 
+  // Method to handle Email/Password Registration
+  Future<void> _handleEmailSignUp() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+        // Navigate to home page on successful registration
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } catch (error) {
+        print("Email Sign-Up failed: $error");
+        // Show error to user
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,178 +95,170 @@ class _RegisterScreenState extends State<RegisterScreen> {
         title: const Text("Create Your Account"),
         elevation: 0,
         centerTitle: true,
-        backgroundColor: Colors.purple[20], // Primary color (Purple)
-        //foregroundColor: Colors.white, // For text color
+        backgroundColor: Colors.purple[200], // Primary color (Purple)
       ),
       body: SafeArea(
         child: Container(
-          //color: Colors.white, // Set background color to white
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Full Name',
-                      style: TextStyle(fontSize: 16, color: Colors.black),
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Full Name',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your full name',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person, color: Color(0xFF9C27B0)), // Primary color (Purple)
                     ),
-                    const SizedBox(height: 8),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Enter your full name',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.person, color: Color(0xFF9C27B0)), // Primary color (Purple)
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your name';
-                        }
-                        return null;
-                      },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Email',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your email address',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email, color: Color(0xFF9C27B0)), // Primary color (Purple)
                     ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'Email',
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                    ),
-                    const SizedBox(height: 8),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Enter your email address',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.email, color: Color(0xFF9C27B0)), // Primary color (Purple)
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'Password',
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                    ),
-                    const SizedBox(height: 8),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter your password',
-                        border: const OutlineInputBorder(),
-                        prefixIcon: const Icon(Icons.lock, color: Color(0xFF9C27B0)), // Primary color (Purple)
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                            color: Colors.black,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordVisible = !_isPasswordVisible;
-                            });
-                          },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Password',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock, color: Color(0xFF9C27B0)), // Primary color (Purple)
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.black,
                         ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
                       ),
-                      obscureText: !_isPasswordVisible,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
                     ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'Confirm Password',
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                    ),
-                    const SizedBox(height: 8),
-                    TextFormField(
-                      controller: _confirmPasswordController,
-                      decoration: InputDecoration(
-                        hintText: 'Re-enter your password',
-                        border: const OutlineInputBorder(),
-                        prefixIcon: const Icon(Icons.lock, color: Color(0xFF9C27B0)), // Primary color (Purple)
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                            color: Colors.black,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
-                            });
-                          },
+                    obscureText: !_isPasswordVisible,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Confirm Password',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      hintText: 'Re-enter your password',
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock, color: Color(0xFF9C27B0)), // Primary color (Purple)
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.black,
                         ),
-                      ),
-                      obscureText: !_isConfirmPasswordVisible,
-                      validator: (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // Handle registration, then navigate to the login page or home
-                          Navigator.pop(context); // Go back to login after registration
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        minimumSize: const Size.fromHeight(50),
-                        backgroundColor: Color(0xFF9C27B0), // Primary color (Purple)
-                      ),
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                          });
+                        },
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: const [
-                        Expanded(child: Divider()),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Text('OR'),
-                        ),
-                        Expanded(child: Divider()),
-                      ],
+                    obscureText: !_isConfirmPasswordVisible,
+                    validator: (value) {
+                      if (value != _passwordController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _handleEmailSignUp,
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      minimumSize: const Size.fromHeight(50),
+                      backgroundColor: Color(0xFF9C27B0), // Primary color (Purple)
                     ),
-                    const SizedBox(height: 20),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: const [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text('OR'),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
 
-                    // Google Registration Button
-                    ElevatedButton.icon(
-                      onPressed: _handleGoogleSignUp, // Handle Google registration
-                      icon: Image.asset('assets/google.png', height: 24, width: 24), // Use the official Google logo
-                      label: const Text('Register with Google'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white, // Secondary color (Blue-Green/Cyan)
-                        padding: const EdgeInsets.symmetric(vertical: 14.0),
-                        minimumSize: const Size.fromHeight(50),
-                      ),
+                  // Google Registration Button
+                  ElevatedButton.icon(
+                    onPressed: _handleGoogleSignUp, // Handle Google registration
+                    icon: Image.asset('assets/google.png', height: 24, width: 24), // Use the official Google logo
+                    label: const Text('Register with Google'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, // Secondary color (Blue-Green/Cyan)
+                      padding: const EdgeInsets.symmetric(vertical: 14.0),
+                      minimumSize: const Size.fromHeight(50),
                     ),
-                    const SizedBox(height: 10),
+                  ),
+                  const SizedBox(height: 10),
 
-                    // Facebook Registration Button
-                    ElevatedButton.icon(
-                      onPressed: _handleFacebookSignUp, // Handle Facebook registration
-                      icon: Image.asset('assets/facebook.png', height: 24, width: 24), // Use the official Facebook logo
-                      label: const Text('Register with Facebook'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        padding: const EdgeInsets.symmetric(vertical: 14.0),
-                        minimumSize: const Size.fromHeight(50),
-                      ),
+                  // Facebook Registration Button
+                  ElevatedButton.icon(
+                    onPressed: _handleFacebookSignUp, // Handle Facebook registration
+                    icon: Image.asset('assets/facebook.png', height: 24, width: 24), // Use the official Facebook logo
+                    label: const Text('Register with Facebook'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 14.0),
+                      minimumSize: const Size.fromHeight(50),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
